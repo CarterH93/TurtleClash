adb912a8cb00315e67337c8156a35e550fc36dd8
CarterH93 <Carterhawkins93@gmail.com>
2024-01-19 09:57:08 -0500

    Fixed rotating glitch

--- CardiMessageGame MessagesExtension/Expanded Views/FinalWrappedExpandedView.swift
+++ CardiMessageGame MessagesExtension/Expanded Views/FinalWrappedExpandedView.swift
@@ -12,33 +12,39 @@ struct FinalWrappedExpanded: View {
     @EnvironmentObject var storage: AppStorage
     var body: some View {
         GeometryReader { geo in
+            if storage.settingsMenuActive == false {
+                WrappedExpanded()
+            }
             if geo.size.height > geo.size.width {
                 
                 
                 if storage.settingsMenuActive == true {
-                    Settings()
+                        Color.black.ignoresSafeArea()
+                        
+                    
                         .frame(width: geo.size.width, height: geo.size.width)
                         .frame(width: geo.size.width, height: geo.size.height)
                     
                     
-                } else {
-                    WrappedExpanded()
-                
                 }
             } else {
-                VStack {
-                    Image("Rotate")
-                        .resizable()
-                        .scaledToFit()
-                        .foregroundColor(.primary)
-                        .frame(width: geo.size.width / 2, height: geo.size.height / 3)
-                    
-                    Text("Please Rotate to Portrait")
-                        .font(.title)
+                ZStack {
+                    Color.black.ignoresSafeArea()
+                    VStack {
+                        Image("Rotate")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.primary)
+                            .frame(width: geo.size.width / 2, height: geo.size.height / 3)
                         
-                }
-                .frame(width: geo.size.width, height: geo.size.width)
+                        Text("Please Rotate to Portrait")
+                            .font(.title)
+                        
+                    }
+                    
+                    .frame(width: geo.size.width, height: geo.size.width)
                     .frame(width: geo.size.width, height: geo.size.height)
+                }
             }
             
         }
